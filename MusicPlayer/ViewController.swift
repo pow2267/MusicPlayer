4a98268c2fc6347fa059c67c55525a85f834f7d1
pow2267 <pow2267@gmail.com>
2021-07-16 10:50:37 +0900

    Add button, label, slider and Connect them with methods

--- MusicPlayer/ViewController.swift
+++ MusicPlayer/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var playPauseButton: UIButton!
+    @IBOutlet var timeLabel: UILabel!
+    @IBOutlet var progressSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func touchUpPlayPauseButton(_ sender: UIButton) {
+        print("button tapped")
+     }
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        print("slider value changed")
+    }
 }
 
